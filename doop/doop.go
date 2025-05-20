b715ff0a9c807f3867410b9ca04e67e35c0998f9
moukanae75 <moukanae75@gmail.com>
2025-05-20 22:42:59 +0100

    last-updat

--- doop/doop.go
+++ doop/doop.go
@@ -1,78 +1,133 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
+const (
+	MaxInt64 = 9223372036854775807
+	MinInt64 = -9223372036854775808
+)
+
 func main() {
+
 	if len(os.Args) != 4 {
 		return
 	}
-	arg1 := os.Args[1]
-	arg2 := os.Args[2]
-	arg3 := os.Args[3]
 
-	if !isNumeric(arg1) || !isNumeric(arg3) {
+	num1, err1 := Atoi(os.Args[1])
+	op := os.Args[2]
+	num2, err2 := Atoi(os.Args[3])
+
+	if !err1 || !err2 {
 		return
 	}
-	res := 0
-	switch arg2 {
+
+	switch op {
 	case "+":
-		res = Atoi(arg1) + Atoi(arg3)
+
+		if (num1 > 0 && num2 > 0 && num1 > MaxInt64-num2) ||
+			(num1 < 0 && num2 < 0 && num1 < MinInt64-num2) {
+			return
+		}
+		PrintNbr(int(num1) + int(num2))
+
 	case "-":
-		res = Atoi(arg1) - Atoi(arg3)
-	case "/":
-		if arg3 == "0" {
-			os.Stdout.WriteString("No division by 0")
-			return
-		}
-		res = Atoi(arg1) / Atoi(arg3)
-	case "%":
-		if arg3 == "0" {
-			os.Stdout.WriteString("No modulo by 0")
-			return
-		}
-		res = Atoi(arg1) / Atoi(arg3)
 
+		if (num1 > 0 && num2 < 0 && num1 > MaxInt64+num2) ||
+			(num1 < 0 && num2 > 0 && num1 < MinInt64+num2) {
+			return
+		}
+		PrintNbr(int(num1) - int(num2))
 	case "*":
-		res = Atoi(arg1) * Atoi(arg3)
 
-	}
-	if atoi >  
-	os.Stdout.WriteString(itoa(res))
-}
-
-func Atoi(s string) int {
-	result := 0
-	t := 1
-	for i, c := range s {
-		if (c == '-' || c == '+') && i == 0 {
-			if c == '-' {
-				t = -1
-			}
-			continue
-		} else if c < '0' || c > '9' {
-			return 0
+		if num1 == 0 || num2 == 0 {
+			os.Stdout.WriteString("0")
+			return
 		}
-		result = result*10 + int(c-'0')
-	}
-	return result * t
-}
 
-func isNumeric(s string) bool {
-	for _, c := range s {
-		if c < '0' || c > '9' {
-			return false
+		maxAllowed := MaxInt64 / abs(int64(num1))
+		minAllowed := MinInt64 / abs(int64(num1))
+
+		if (num1 > 0 && num2 > 0 && num2 > int(maxAllowed)) ||
+			(num1 < 0 && num2 < 0 && num2 < int(minAllowed)) ||
+			(num1 > 0 && num2 < 0 && num2 < int(minAllowed)) ||
+			(num1 < 0 && num2 > 0 && num2 > int(maxAllowed)) {
+			return
 		}
+		PrintNbr(int(num1) * int(num2))
+
+	case "/":
+
+		if num2 == 0 {
+			fmt.Println("No division by 0")
+			return
+		}
+
+		if num1 == MinInt64 && num2 == -1 {
+			return
+		}
+		PrintNbr(int(num1) / int(num2))
+
+	case "%":
+
+		if num2 == 0 {
+			fmt.Println("No modulo by 0")
+			return
+		}
+
+		if num1 == MinInt64 && num2 == -1 {
+			return
+		}
+		PrintNbr(int(num1) % int(num2))
+
+	default:
+		return
 	}
-	return true
 }
 
-func itoa(n int) string {
+func abs(n int64) int64 {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func PrintNbr(n int) {
+
 	str := ""
-	for n >= 0 {
-		str = string(n%10 + '0')
+	last := ""
+	if n < 0 {
+		n = -n
+		last = "-"
+
+	}
+	for n > 0 {
+		str = string(n%10+48) + str
 		n /= 10
 	}
-	return str
+	last += str
+	os.Stdout.WriteString(last)
+}
+func Atoi(s string) (int, bool) {
+	nbr := 0
+	if len(s) > 0 {
+		factor := 1
+		for i := len(s) - 1; i >= 0; i-- {
+			if s[i] < '0' || s[i] > '9' {
+				if i != 0 || (s[0] != '-' && s[0] != '+') {
+					return 0, false
+				}
+			}
+			if s[i] != '-' && s[i] != '+' {
+				nbr += (int(s[i]) - 48) * factor
+				factor = factor * 10
+			}
+		}
+		if s[0] == '-' {
+			return -nbr, true
+		}
+	}
+	return nbr, true
 }
